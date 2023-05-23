c41db3cc1ce4f845b06587cdc5b55035c25d8f0e
Ji <68333747+An-JIeun@users.noreply.github.com>
2023-05-23 13:29:07 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import MainPageView from '@/components/HomeView.vue';
 import PofolView from "@/components/PofolView.vue";
 import HomeView from "@/components/EnterView.vue"
@@ -33,7 +33,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
